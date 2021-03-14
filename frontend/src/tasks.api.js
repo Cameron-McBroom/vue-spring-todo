0022dca5b14697540953a7609b9dae744514b91d
Cameron <cam.mcbroom@gmail.com>
2021-03-14 20:04:04 +1100

    Slight changes to config

--- frontend/src/tasks.api.js
+++ frontend/src/tasks.api.js
@@ -2,34 +2,34 @@ import {AXIOS} from "./http-common"
 
 export default {
     async getTaskById(id) {
-        const response = await AXIOS.get(`api/task/${id}`);
+        const response = await AXIOS.get(`/task/${id}`);
         return response.data;
     },
 
     async getTasks() {
-        const response = await AXIOS.get("api/task");
+        const response = await AXIOS.get("/task");
         return response.data;
     },
 
     async deleteTaskById(id) {
-        const response = await AXIOS.delete(`api/task/${id}`);
+        const response = await AXIOS.delete(`/task/${id}`);
         return response.data
     },
 
     async addNewTask(task) {
-        const response = await AXIOS.post('api/task', {
+        const response = await AXIOS.post('/task', {
             name: task.name
         })
         return response.data
     },
 
     async markComplete(id, complete) {
-        const response = await AXIOS.put(`api/task/${id}/${complete.toLocaleString().toLocaleLowerCase()}`)
+        const response = await AXIOS.put(`/task/${id}/${complete.toLocaleString().toLocaleLowerCase()}`)
         return response.data;
     },
 
     async updateTaskName(id, newName) {
-      const response = await AXIOS.post(`api/task/${id}/update`, {
+      const response = await AXIOS.post(`/task/${id}/update`, {
           name: newName
       })
         return response.data
